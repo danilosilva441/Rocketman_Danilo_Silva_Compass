0d9d0c5b493fd5c57c07252349252df2bc995444
Danilo da Silva Souza <danilosilva441@gmail.com>
2022-06-02 12:01:41 -0300

    Exercicio 2

---
+++ AtividadesRocketman/Exercicio 2/index.js
@@ -0,0 +1,44 @@
+//Exemplo da Aula
+
+// let nome = 'Rafael'; //String literal
+// let idade = 25; //Number literal
+// let estaAprovado = true; //Boolean 
+// let sobreNome = undefined; //Undefined
+// let corSelecionado = null; //Redefinir um valor
+
+// let pessoa = {
+//     nome: 'Rafael',
+//     idade: 25,
+//     estaAprovado: true,
+//     sobreNome: 'de Souza'
+// };
+
+// console.log(pessoa);
+
+
+//-------------------------------------------------------------------------
+
+
+//Exercicio 2
+
+let nome = 'Danilo';
+let sobreNome = 'S Souza';
+let idade = 27;
+let altura = 1.82;
+let cursando = 'CC';
+let cursoStatus = true;
+let estagio = 'Compass';
+let estagioStatus = true;
+
+let ficha = {
+    nome: 'Danilo',
+    sobreNome: 'S Souza',
+    idade: 27,
+    altura: 1.82,
+    cursando: 'CC',
+    cursoStatus: true,
+    estagio: 'Compass',
+    estagioStatus: true
+};
+
+console.log(ficha);
